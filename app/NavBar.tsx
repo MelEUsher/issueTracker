81a6bbd42a4e12243e9eeb0e5592f0ff3713ac6b
Melissa Usher <MelEUsher17@gmail.com>
2025-02-20 14:01:59 -0600

    add bug logo

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/">Logo</Link> 
+      <Link href="/"><FaBug /></Link> 
       <ul className='flex space-x-6'>
         <li><Link href="/">Dashboard</Link></li>
         <li><Link href="/issues">Issues</Link></li>
