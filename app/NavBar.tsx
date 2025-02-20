5d083f0ec8bdc6b0aa86a14728ec00fd8207a89a
Melissa Usher <MelEUsher17@gmail.com>
2025-02-20 14:06:30 -0600

    transition applied to dashboard in nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href="/"><FaBug /></Link> 
       <ul className='flex space-x-6'>
-        <li><Link href="/">Dashboard</Link></li>
+        <li><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href="/">Dashboard</Link></li>
         <li><Link href="/issues">Issues</Link></li>
       </ul>
     </nav>
