ffd9b331f2ce8d49b23290b563861683c30608d0
Melissa Usher <MelEUsher17@gmail.com>
2025-02-20 14:59:37 -0600

    clean up classname key value pairs

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -22,7 +22,11 @@ const NavBar = () => {
         {links.map(link => 
         <Link 
           key={link.href} 
-          className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors` }
+          className={classnames ({
+            'text-zinc-900': link.href === currentPath,
+            'text-zinc-500': link.href !== currentPath
+            'hover:text-zinc-800 transition-colors': true
+          })}
           href={link.href}>{link.label}</Link>)}
       </ul>
     </nav>
